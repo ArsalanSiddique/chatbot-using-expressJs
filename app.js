637083edcf1934c3944d66ad463b6c1b6476f979
arsalan.ahmed1264@gmail.com <arsalan.ahmed1264@gmail.com>
2019-08-08 04:23:42 +0500

    weatherBot is underdevelopment

--- app.js
+++ app.js
@@ -17,39 +17,38 @@ app.post("/webhook", function (request, response, next) {
     function weather(agent) {
         
         var cityName = agent.parameters.city;
-        agent.add(`Working properly ${cityName}`);
-        // var weatherApi = 'aeef3d2ed53e72fbe6c0a8309db31f61';
-        // var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${weatherApi}`;
+        var weatherApi = 'aeef3d2ed53e72fbe6c0a8309db31f61';
+        var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${weatherApi}`;
 
-        // if (cityName) {
-        //     req.get(url, function (err, response, body) {
-        //         if (err) {
-        //             console.log("Error:", err);
-        //             agent.add("Error! while getting weather info from server, try again.")
-        //         } else {
-        //             var weather = JSON.parse(body)
-        //             if (weather.main == undefined) {
-        //                 agent.add("Something went wrong, try agian.");
-        //             } else {
-        //                 var temCelcius = Math.round(((weather.main.temp - 32) * 5 / 9));
-        //                 console.log("temp: ", temCelcius)
-        //                 var weatherTemp = `${temCelcius}`;
-        //                 var name = `${weather.name}`;
-        //                 console.log("Name: ", name)
-        //                 var weatherTxt = 'It is ' + `${temCelcius}` + '&#8451; in ' + `${weather.name}` + '.';
-        //                 console.log("Message: ", weatherTxt)
+        if (cityName) {
+            req.get(url, function (err, response, body) {
+                if (err) {
+                    console.log("Error:", err);
+                    agent.add("Error! while getting weather info from server, try again.")
+                } else {
+                    var weather = JSON.parse(body)
+                    if (weather.main == undefined) {
+                        agent.add("Something went wrong, try agian.");
+                    } else {
+                        var temCelcius = Math.round(((weather.main.temp - 32) * 5 / 9));
+                        console.log("temp: ", temCelcius)
+                        var weatherTemp = `${temCelcius}`;
+                        var name = `${weather.name}`;
+                        console.log("Name: ", name)
+                        var weatherTxt = 'It is ' + `${temCelcius}` + '&#8451; in ' + `${weather.name}` + '.';
+                        console.log("Message: ", weatherTxt)
                         
-        //             }
-        //             agent.add(`${weatherTxt} - temperature: ${weatherTemp}, City: ${name}`);
-        //             console.log('Success')
-        //             return;
-        //         }
+                    }
+                    agent.add(`${weatherTxt} - temperature: ${weatherTemp}, City: ${name}`);
+                    console.log('Success')
+                    return;
+                }
 
-        //     });
-        // } else {
-        //     agent.add(`Please Mention your city here `);
-        //     return;
-        // }
+            });
+        } else {
+            agent.add(`Please Mention your city here `);
+            return;
+        }
 
     }
 
