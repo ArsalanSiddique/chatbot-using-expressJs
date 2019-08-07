bcce7d97bc68a2ecf4afecae551e203833e06926
arsalan.ahmed1264@gmail.com <arsalan.ahmed1264@gmail.com>
2019-08-08 03:10:59 +0500

    weatherBot is underdevelopment

--- app.js
+++ app.js
@@ -1,35 +1,71 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+// var express = require("express");
+// var bodyParser = require("body-parser");
+// var request = require("request");
+// const { WebhookClient } = require("dialogflow-fulfillment");
+
+// const app = express().use(bodyParser.json());
+
+// app.get("/", function (request, response) {
+//     response.send("Hello world!");
+// });
+
+// app.post("/webhook", function (request, response, next) {
+//     const agent = new webHookClient({ request: request, response: response });
+
+
+//     function welcome(agent) {
+//         agent.add("Hello! I am weatherBot, Want to know the weather?");
+//         agent.add("Please type your city name");
+//     }
+
+//     function fallback() {
+//         agent.add("Sorry! I didn't understand.");
+//         agent.add("Please type again.");
+//     }
+
+//     let intentMap = new Map();
+//     intentMap.set("Default Welcome Intent", welcome);
+//     intentMap.set("Default Fallback Intent", fallback);
+
+//     agent.handleRequest(intentMap);
+
+// });
+// app.listen(process.env.PORT || 3001, function () {
+//     console.log("app is running in 3000");
+// });
+
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
 var request = require("request");
+
 const { WebhookClient } = require("dialogflow-fulfillment");
 
-const app = express().use(bodyParser.json());
 
-app.get("/", function (request, response) {
-    response.send("Hello world!");
-});
-
-app.post("/webhook", function (request, response, next) {
-    const agent = new webHookClient({ request: request, response: response });
+let apiKey='4970e4f266675063af77ad454f45ebd6';
 
 
-    function welcome(agent) {
-        agent.add("Hello! I am weatherBot, Want to know the weather?");
-        agent.add("Please type your city name");
-    }
+const expressApp = express().use(bodyParser.json());
 
-    function fallback() {
-        agent.add("Sorry! I didn't understand.");
-        agent.add("Please type again.");
-    }
+expressApp.post("/webhook", function (request, response, next) {
+  const agent = new WebhookClient({ request: request, response: response });
 
-    let intentMap = new Map();
-    intentMap.set("Default Welcome Intent", welcome);
-    intentMap.set("Default Fallback Intent", fallback);
+  function welcome(agent) {
+    agent.add(`Good day! What can I do for you today?`);
+  }
 
-    agent.handleRequest(intentMap);
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+  let intentMap = new Map();
+  intentMap.set("Default Welcome Intent", welcome);
+  intentMap.set("Default Fallback Intent", fallback);
+
+  agent.handleRequest(intentMap);
 
 });
-app.listen(process.env.PORT || 3001, function () {
-    console.log("app is running in 3000");
+expressApp.listen(process.env.PORT || 3000, function () {
+  console.log("app is running in 3000");
 });
