433e7c8b4c21a072c7c57305b871a233aab7a436
arsalan.ahmed1264@gmail.com <arsalan.ahmed1264@gmail.com>
2019-08-08 04:27:13 +0500

    weatherBot is underdevelopment

--- app.js
+++ app.js
@@ -21,32 +21,34 @@ app.post("/webhook", function (request, response, next) {
         var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${weatherApi}`;
 
         if (cityName) {
-            req.get(url, function (err, response, body) {
-                if (err) {
-                    console.log("Error:", err);
-                    agent.add("Error! while getting weather info from server, try again.")
-                } else {
-
-                    // // var weather = JSON.parse(body)
-                    // // if (weather.main == undefined) {
-                    // //     agent.add("Something went wrong, try agian.");
-                    // // } else {
-                    // //     var temCelcius = Math.round(((weather.main.temp - 32) * 5 / 9));
-                    // //     console.log("temp: ", temCelcius)
-                    // //     var weatherTemp = `${temCelcius}`;
-                    // //     var name = `${weather.name}`;
-                    // //     console.log("Name: ", name)
-                    // //     var weatherTxt = 'It is ' + `${temCelcius}` + '&#8451; in ' + `${weather.name}` + '.';
-                    // //     console.log("Message: ", weatherTxt)
+            // req.get(url, function (err, response, body) {
+            //     if (err) {
+            //         console.log("Error:", err);
+            //         agent.add("Error! while getting weather info from server, try again.")
+            //     } else {
+                    
+            //         var weather = JSON.parse(body)
+            //         if (weather.main == undefined) {
+            //             agent.add("Something went wrong, try agian.");
+            //         } else {
+            //             var temCelcius = Math.round(((weather.main.temp - 32) * 5 / 9));
+            //             console.log("temp: ", temCelcius)
+            //             var weatherTemp = `${temCelcius}`;
+            //             var name = `${weather.name}`;
+            //             console.log("Name: ", name)
+            //             var weatherTxt = 'It is ' + `${temCelcius}` + '&#8451; in ' + `${weather.name}` + '.';
+            //             console.log("Message: ", weatherTxt)
                         
-                    // // }
-                    // agent.add(`${weatherTxt} - temperature: ${weatherTemp}, City: ${name}`);
-                    // console.log('Success')
-                    agent.add(`URL: ${url}`);
-                    return;
-                }
+            //         }
+            //         agent.add(`${weatherTxt} - temperature: ${weatherTemp}, City: ${name}`);
+            //         console.log('Success')
+            //         return;
+            //     }
 
-            });
+            // });
+
+
+            agent.add(`URL: ${url}`);
         } else {
             agent.add(`Please Mention your city here `);
             return;
