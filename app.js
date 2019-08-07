da670a197cc76767a7c3ada71cadc3f27fff9be8
arsalan.ahmed1264@gmail.com <arsalan.ahmed1264@gmail.com>
2019-08-08 03:04:43 +0500

    weatherBot is underdevelopment

--- app.js
+++ app.js
@@ -3,34 +3,33 @@ var bodyParser = require("body-parser");
 var request = require("request");
 const { WebhookClient } = require("dialogflow-fulfillment");
 
-var app = express().use(bodyParser.json());
+const app = express().use(bodyParser.json());
 
 app.get("/", function (request, response) {
     response.send("Hello world!");
 });
 
-app.post("/webhook", function (req, res, next) {
-    const agent = new webHookClient({ request: req, response: res });
+app.post("/webhook", function (request, response, next) {
+    const agent = new webHookClient({ request: request, response: response });
+
+
+    function welcome(agent) {
+        agent.add("Hello! I am weatherBot, Want to know the weather?");
+        agent.add("Please type your city name");
+    }
+
+    function fallback() {
+        agent.add("Sorry! I didn't understand.");
+        agent.add("Please type again.");
+    }
+
+    let intentMap = new Map();
+    intentMap.set("Default Welcome Intent", welcome);
+    intentMap.set("Default Fallback Intent", fallback);
+
+    agent.handleRequest(intentMap);
+
 });
-
-function welcome(agent) {
-    agent.add("Hello! I am weatherBot, Want to know the weather?");
-    agent.add("Please type your city name");
-}
-
-function fallback() {
-    agent.add("Sorry! I didn't understand.");
-    agent.add("Please type again.");
-}
-
-
-let intentMap = new Map();
-
-intentMap.set("Default Welcome Intent", welcome);
-intentMap.set("Default Fallback Intent", fallback);
-
-agent.handleRequest(intentMap);
-
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3001, function () {
     console.log("app is running in 3000");
 });
