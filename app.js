709d3f518c903398b859aa74819149c0d5b30c55
arsalan.ahmed1264@gmail.com <arsalan.ahmed1264@gmail.com>
2019-08-08 03:16:48 +0500

    weatherBot is underdevelopment

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var request = require("request");
-const { WebhookClient } = require("dialogflow-fulfillment");
+
+const { WebHookClient } = require("dialogflow-fulfillment");
 
 const app = express().use(bodyParser.json());
 
+app.get("/", function (request, response) {
+    response.send("Hello world!");
+});
+
 app.post("/webhook", function (request, response, next) {
-    const agent = new webHookClient({ request: request, response: response });
+    const agent = new WebHookClient({ request: request, response: response });
 
 
     function welcome(agent) {
@@ -30,39 +35,3 @@ app.listen(process.env.PORT || 3001, function () {
     console.log("app is running in 3000");
 });
 
-
-//==========  Abrar code start here //
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// var request = require("request");
-
-// const { WebhookClient } = require("dialogflow-fulfillment");
-
-
-// let apiKey='4970e4f266675063af77ad454f45ebd6';
-
-
-// const expressApp = express().use(bodyParser.json());
-
-// expressApp.post("/webhook", function (request, response, next) {
-//   const agent = new WebhookClient({ request: request, response: response });
-
-//   function welcome(agent) {
-//     agent.add(`Good day! What can I do for you today?`);
-//   }
-
-//   function fallback(agent) {
-//     agent.add(`I didn't understand`);
-//     agent.add(`I'm sorry, can you try again?`);
-//   }
-//   let intentMap = new Map();
-//   intentMap.set("Default Welcome Intent", welcome);
-//   intentMap.set("Default Fallback Intent", fallback);
-
-//   agent.handleRequest(intentMap);
-
-// });
-// expressApp.listen(process.env.PORT || 3000, function () {
-//   console.log("app is running in 3000");
-// });
