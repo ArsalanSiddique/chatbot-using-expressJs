93121b6c288bb579c70ab5f80dbd6203c27eeace
arsalan.ahmed1264@gmail.com <arsalan.ahmed1264@gmail.com>
2019-08-08 04:41:31 +0500

    weatherBot is underdevelopment

--- app.js
+++ app.js
@@ -16,73 +16,41 @@ app.post("/webhook", function (request, response, next) {
 
 
     function weather(agent) {
-
+        
         var cityName = agent.parameters.city;
         var weatherApi = 'aeef3d2ed53e72fbe6c0a8309db31f61';
         var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${weatherApi}`;
 
-        rp.get(url, function (err, response, body) {
+        rp.get(url, function(err, response, body) {
             if (err) {
-                console.log("error:", err);
-                agent.add("Error while getting weather report");
+              console.log("error:", err);
+              agent.add("Error while getting weather report");
             } else {
-                let weather = JSON.parse(body);
-                let message = `${weather.main.temp} degrees and ${
-                    weather.main.humidity
-                    } humidity !`;
-                console.log("weather:", message);
-                let temp = weather.main.temp;
-                console.log("temperature:", temp);
-
-                console.log("City Name:", cityName);
-                agent.setContext({
-                    name: "location",
-                    lifespan: 5,
-                    parameters: { contextcity: cityName }
-                });
-                agent.add(`The weather for the city ${cityName} is: ${message} `);
-                console.log("Success:");
-                return;
+              let weather = JSON.parse(body);
+              let message = `${weather.main.temp} degrees and ${
+                weather.main.humidity
+              } humidity !`;
+              console.log("weather:", message);
+              let temp = weather.main.temp;
+              console.log("temperature:", temp);
+      
+              console.log("City Name:", cityName);
+              agent.setContext({
+                name: "location",
+                lifespan: 5,
+                parameters: { contextcity: cityName }
+              });
+              agent.add(`The weather for the city ${cityName} is: ${message} `);
+              console.log("Success:");
+              return;
             }
-        });
+          });
     }
 
 
     function welcome(agent) {
         agent.add("Hello! I am weatherBot, Want to know the weather?");
         agent.add("Please type your city name");
-
-
-        var city = "karachi";
-        var weatherApi = 'aeef3d2ed53e72fbe6c0a8309db31f61';
-        var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${weatherApi}`;
-
-        if (city) {
-            req.get(url, function (err, response, body) {
-                if (err) {
-                    console.log("Error:", err);
-                    agent.add("Error! while getting weather info from server, try again.")
-                } else {
-                    var weather = JSON.parse(body)
-                    if (weather.main == undefined) {
-                        agent.add("Something went wrong, try agian.");
-                    } else {
-                        var temCelcius = Math.round(((weather.main.temp - 32) * 5 / 9));
-                        console.log("temp: ", temCelcius)
-                        var weatherTemp = `${temCelcius}`;
-                        var name = `${weather.name}`;
-                        console.log("Name: ", name)
-                        var weatherTxt = 'It is ' + `${temCelcius}` + '&#8451; in ' + `${weather.name}` + '.';
-                        console.log("Message: ", weatherTxt)
-
-                    }
-                    agent.add(`${weatherTxt} - temperature: ${weatherTemp}, City: ${name}`);
-                    console.log('Success')
-                    return;
-                }
-
-            });
-        }
     }
 
     function fallback() {
