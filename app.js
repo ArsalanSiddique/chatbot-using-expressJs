64435db2a685cd694e799b4504dd2d48efc3227b
arsalan.ahmed1264@gmail.com <arsalan.ahmed1264@gmail.com>
2019-08-08 04:11:14 +0500

    weatherBot is underdevelopment

--- app.js
+++ app.js
@@ -56,6 +56,37 @@ app.post("/webhook", function (request, response, next) {
     function welcome(agent) {
         agent.add("Hello! I am weatherBot, Want to know the weather?");
         agent.add("Please type your city name");
+
+
+        var city = "karachi";
+        var weatherApi = 'aeef3d2ed53e72fbe6c0a8309db31f61';
+        var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${weatherApi}`;
+
+        if (city) {
+            req.get(url, function (err, response, body) {
+                if (err) {
+                    console.log("Error:", err);
+                    agent.add("Error! while getting weather info from server, try again.")
+                } else {
+                    var weather = JSON.parse(body)
+                    if (weather.main == undefined) {
+                        agent.add("Something went wrong, try agian.");
+                    } else {
+                        var temCelcius = Math.round(((weather.main.temp - 32) * 5 / 9));
+                        console.log("temp: ", temCelcius)
+                        var weatherTemp = `${temCelcius}`;
+                        var name = `${weather.name}`;
+                        console.log("Name: ", name)
+                        var weatherTxt = 'It is ' + `${temCelcius}` + '&#8451; in ' + `${weather.name}` + '.';
+                        console.log("Message: ", weatherTxt)
+                        
+                    }
+                    agent.add(`${weatherTxt} - temperature: ${weatherTemp}, City: ${name}`);
+                    console.log('Success')
+                    return;
+                }
+
+            });
     }
 
     function fallback() {
